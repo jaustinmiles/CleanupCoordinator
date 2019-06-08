48792da7a0e22c7c907dd4fabe6c30b76872036b
Austin Miles <jmiles35@gatech.edu>
2019-06-08 12:07:47 -0400

    commit to heroku. All files

---
+++ add_housing_access.py
@@ -0,0 +1,15 @@
+from app import db, User
+"""
+Use offline to add housing manager to database. This file should never be added to Github or any public repository.
+"""
+if __name__ == '__main__':
+    email = input("Enter the email to add: ")
+    username = input("Enter the username to add: ")
+    password = input("Enter your password: ")
+    pass_confirm = input("Please re-input your password for confirmation: ")
+    while pass_confirm != password:
+        password = input("Passwords did not match. Try again or exit to restart: ")
+        pass_confirm = input("Confirm your password: ")
+    user = User(email, username, password)
+    db.session.add(user)
+    db.session.commit()
